17819fee568c76d87d317b480c2608312fdb33db
Jenbo <oliverjenbo677@gmail.com>
2025-11-14 18:22:23 +0100

    tester om husky pre-push virker

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  // Du kan skifte til "jsdom" senere, hvis du vil teste React-komponenter
+  testEnvironment: 'node',
+
+  // Hvor Jest skal lede efter tests
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+
+  // Almindelige filtyper i dit projekt
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+
+  // Rydder mocks mellem tests (god standard)
+  clearMocks: true,
+
+  // VIGTIGT: sørger for at Jest ikke fejler, selvom du ikke har tests endnu
+  passWithNoTests: false,
+};
