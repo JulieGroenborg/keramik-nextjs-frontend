4996ae04d88172adfbb22f1bb948f2d7dc4c7e6c
Jenbo <oliverjenbo677@gmail.com>
2025-11-14 15:16:36 +0100

    Opsat prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true, // sørger for '' istedetfor ""
+  trailingComma: 'es5', // ligger komma på siste element i lister og objekter
+  semi: true, // Sørger for semikolon ved slutningen af linjer
+  printWidth: 100, // Maksimal linjelængde på 100 tegn
+  tabWidth: 2, // indrykning med svarende til 2 mellemrum
+  bracketSpacing: true, // Sørger for mellemrum i objekter
+  arrowParens: 'always', // sørger for at der altid er parenteser omkring arrow function parametre
+};
