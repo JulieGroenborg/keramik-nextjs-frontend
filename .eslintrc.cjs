1c5cd8a9ec37df2b34cbac89529b6bbe4cc94723
Jenbo <oliverjenbo677@gmail.com>
2025-11-14 15:18:23 +0100

    Opsat ESlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true, // klar til tests senere
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended', // kobler Prettier på
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn', // viser Prettier-fejl som ESLint-warnings
+    'react/react-in-jsx-scope': 'off', // Next.js behøver ikke import React
+    'no-var': 'error', // forbyder brug af var
+    'prefer-const': 'warn', // opfordrer til brug af const når muligt
+    'no-duplicate-imports': 'error', // forbyder duplikerede imports
+
+    'react/jsx-boolean-value': ['warn', 'never'], // undgå {true} i props
+    'react/jsx-curly-spacing': ['warn', { when: 'never', children: true }], // ingen mellemrum i {}
+    'react/jsx-indent': ['warn', 2], // 2 mellemrum indrykning i JSX
+    'react/jsx-indent-props': ['warn', 2], // 2 mellemrum indrykning for props i JSX
+    'react/jsx-no-useless-fragment': 'warn', // undgå unødvendige fragments
+    'react/self-closing-comp': 'warn', // brug selv-lukkende tags når muligt
+
+    'react-hooks/rules-of-hooks': 'error', // sikrer korrekt brug af hooks
+    'react-hooks/exhaustive-deps': 'warn', // sikrer dependency array i hooks
+  },
+};
