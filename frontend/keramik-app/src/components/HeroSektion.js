2d2e663a52566883c2ba61ab92d020a89a028cae
sebg22 <sebmp48@gmail.com>
2025-12-02 17:02:48 +0100

    standardopsætning til komponenter

--- frontend/keramik-app/src/components/HeroSektion.js
+++ frontend/keramik-app/src/components/HeroSektion.js
@@ -2,15 +2,14 @@ import styles from '../css/components/HeroSektion.module.css';
 
 export default function HeroSektion({ page }) {
   const title = page?.properties?.title.trim();
+  const subtitle = page?.properties?.subtitle;
 
   return (
     <div className={styles.hero}>
       <div className="container">
         {title && <h1>{title}</h1>}
 
-        {/* subtitle*/}
-
-        {/* import reuseable knap */}
+        {subtitle && <p className={styles.p}>{subtitle}</p>}
       </div>
     </div>
   );
