533a62c18ee1eca9a5e86af210f7ab1c6c30f02a
sebg22 <sebmp48@gmail.com>
2025-12-02 12:48:34 +0100

    korrekt filstruktur samt, et test komponent

---
+++ frontend/keramik-app/src/components/HeroSektion.js
@@ -0,0 +1,18 @@
+import '../css/components/HeroSektion.css';
+import '../css/stylesheet.css';
+
+export default function HeroSektion({ page }) {
+  const title = page?.properties?.title.trim();
+
+  return (
+    <div className="container">
+      {title && <h1>{title}</h1>}
+
+      {/* subtitle*/}
+
+      {/* import reuseable knap */}
+
+      {/* billede */}
+    </div>
+  );
+}
