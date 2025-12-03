dd64ec5c250dfdd15fdaace9b7467f15fd05dd0d
JulieGroenborg <julieeriksen09@hotmail.com>
2025-12-03 12:04:40 +0100

    Lavet fetch til shop-siden, fetcher categorier og produkter

---
+++ frontend/keramik-app/src/lib/umbraco/api-helpers/getShop.js
@@ -0,0 +1,22 @@
+// GET all products and categories in shop.
+// // https://localhost:51857/umbraco/delivery/api/v2/content?fetch=descendants:/shop
+export async function getShop() {
+  // Fetch alle descendants under /shop.
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_UMBRACO_URL}/content?fetch=descendants:/shop`,
+    { cache: 'no-store' } //no-store betyder, at next IKKE vil cache dette request, men at al data altid er friskt.
+  );
+
+  if (!res.ok) throw new Error('Failed to fetch shop descendants');
+
+  const { items } = await res.json(); // flat data structure of all categories/products. Et stort array.
+
+  // Separate categories og products into two arrays.
+  const categories = items.filter((i) => i.contentType === 'category');
+  const products = items.filter((i) => i.contentType === 'product');
+
+  return {
+    categories, // all categories
+    products, // all products
+  };
+}
