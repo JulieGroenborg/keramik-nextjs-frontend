377c19af14166d3b5f7ea779f7d27269860041a1
Oliver Martin Husted Jenbo <Oliverjenbo677@gmail.com>
2025-12-03 15:17:20 +0100

    Tillader billeder hentet fra umbraco

--- frontend/keramik-app/next.config.mjs
+++ frontend/keramik-app/next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '51857',
+        pathname: '/**', // or '/**' if you want to allow all paths
+      },
+    ],
+    // ⚠️ Dev-only: allow optimizing images from localhost
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
