1fe7dbafbbe4c6721976e3a8006dd8b42108b10d
JulieGroenborg <julieeriksen09@hotmail.com>
2025-12-08 15:25:16 +0100

    Lavet kurv-siden

--- frontend/keramik-app/src/app/[...slug]/page.js
+++ frontend/keramik-app/src/app/[...slug]/page.js
@@ -3,12 +3,18 @@ import PageLayout from '@/layouts/PageLayout';
 import { notFound } from 'next/navigation';
 
 export default async function SlugPage(props) {
-  // unwrap params fra props
+  // Unwrap params fra props
   const params = await props.params; // <-- det nye i Next.js 16
 
   // Sørg for, at slug altid er et array
   const slugArray = Array.isArray(params?.slug) ? params.slug : [];
   const slugPath = '/' + slugArray.join('/') + '/';
+
+  // Tjekker at URL'en starter med en af de godkendte prefixes, så vi undgår at fetche fra CMS med invalide URL'er.
+  const exsitingPagesInUmbraco = ['/om-mig/', '/kontakt/']; // Add all valid CMS prefixes
+  if (!exsitingPagesInUmbraco.some((prefix) => slugPath.startsWith(prefix))) {
+    notFound(); // or just return null
+  }
 
   // Hent page fra Umbraco
   const page = await getPage(slugPath);
