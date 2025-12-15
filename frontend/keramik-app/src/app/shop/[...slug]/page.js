00c363e8cde31bd130edbdc1ea929456f826c95d
Oliver Martin Husted Jenbo <Oliverjenbo677@gmail.com>
2025-12-15 14:14:12 +0100

    page henter både enkelt produkt og alle produkter

--- frontend/keramik-app/src/app/shop/[...slug]/page.js
+++ frontend/keramik-app/src/app/shop/[...slug]/page.js
@@ -1,6 +1,7 @@
 import { getProductBySlug } from '@/lib/shop/api-helpers/getProductBySlug';
 import { redirect } from 'next/navigation';
 import Product from '@/components/Product';
+import { getShop } from '@/lib/shop/api-helpers/getShop';
 
 export default async function ProductPage({ params }) {
   const { slug } = await params; // Extracts the slug array from params. Slug will be something like ['kopper', 'datter-koppen']
@@ -12,9 +13,13 @@ export default async function ProductPage({ params }) {
 
   const productSlug = slug.at(-1); // Tager den sidste del af URL'en, altså produktet.
 
-  const product = await getProductBySlug(productSlug); // Fetches produktet fra vores API by slug.
+  // promise.all kører begge asynkrone funktioner parallelt og venter på at begge er færdige.
+  const [product, shopData] = await Promise.all([
+    getProductBySlug(productSlug), // Henter det specifikke produkt baseret på slug
+    getShop(), // Henter alle produkter i shoppen (bruges til at finde relaterede produkter)
+  ]);
 
   if (!product) return <div>Produkt ikke fundet</div>;
 
-  return <Product product={product} />;
+  return <Product product={product} allProducts={shopData.products} />;
 }
