fc41d276a062d6f3f8e3cf38cc6b40f295dd7613
Oliver Martin Husted Jenbo <Oliverjenbo677@gmail.com>
2025-12-18 11:54:19 +0100

    prettier igen

--- frontend/keramik-app/src/components/ProductSection.js
+++ frontend/keramik-app/src/components/ProductSection.js
@@ -2,13 +2,7 @@ import ProductCard from './ProductCard';
 import styles from '../css/components/ProductSection.module.css';
 import Button from './Button';
 
-export default function ProductSection({
-  title,
-  products,
-  variant,
-  buttonText,
-  buttonLink,
-}) {
+export default function ProductSection({ title, products, variant, buttonText, buttonLink }) {
   if (!products || products.length === 0) return null;
 
   const containerClass = `${styles.section} ${styles[variant]}`;
