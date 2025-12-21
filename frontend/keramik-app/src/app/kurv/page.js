4e36a7688347ade722630c6a7d9cc9084a1148b6
Oliver Martin Husted Jenbo <Oliverjenbo677@gmail.com>
2025-12-21 16:50:26 +0100

    Cart page render banner og cartview komponentet nu

--- frontend/keramik-app/src/app/kurv/page.js
+++ frontend/keramik-app/src/app/kurv/page.js
@@ -1,11 +1,24 @@
-import { CartDisplay } from '@/components/CartDisplayWrapper';
+'use client';
+import dynamic from 'next/dynamic';
+import styles from '../../css/components/CartPage.module.css'; // Import af den nye CSS
+import Banner from '@/components/Banner';
+
+const CartView = dynamic(() => import('@/components/CartView'), {
+  ssr: false,
+  loading: () => <p style={{ textAlign: 'center', padding: '50px' }}>Indlæser kurv...</p>,
+});
 
 export default function CartPage() {
   return (
     <main>
-      <h1>Din kurv</h1>
-      {/* Enten sender man "checkout" eller "drawer" ned som props til checkout-button*/}
-      <CartDisplay mode={'checkout'} />
+      <div className={styles.headerBanner}>
+        <Banner title="Din kurv" />
+      </div>
+
+      {/* CartView håndterer grid/listen */}
+      <div className={styles.pageContainer}>
+        <CartView />
+      </div>
     </main>
   );
 }
